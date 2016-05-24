7015c6298fe2764b37f0652572c7ca97903045fc
gfed53 <gfederico7153@gmail.com>
2016-05-24 17:24:04 -0400

    Initial commit: header, menu style changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,26 @@
-var gulp = require('gulp');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var imagemin = require("gulp-imagemin");
 
-var imagemin = require('gulp-imagemin');
+
+//SASS
+gulp.task("sass", function(){
+	return gulp.src("site/scss/*.scss")
+	.pipe(sass())
+	.pipe(gulp.dest("site/css"));
+});
 
 // Image optimization task
-gulp.task('images', function() {
-  return gulp.src('site/images/*')
+gulp.task("images", function() {
+  return gulp.src("site/images/*")
     .pipe(imagemin())
-    .pipe(gulp.dest('site/images-min'));
+    .pipe(gulp.dest("site/images-min"));
 });
+
+// Watch task
+gulp.task("watch", function(){
+	gulp.watch("site/scss/*", ["sass"]);
+});
+
+//Default
+gulp.task("default", ["sass", "watch"]);
