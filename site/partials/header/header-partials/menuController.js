fa38bcbeaca9654772c54288d9fb8bc7058c1272
gfed53 <gfederico7153@gmail.com>
2016-05-30 17:31:26 -0400

    Made hamburger icon more responsive, dropdown has slide animation

--- site/partials/header/header-partials/menuController.js
+++ site/partials/header/header-partials/menuController.js
@@ -4,16 +4,25 @@ angular
 
 function MenuCtrl(myAutoscrollToggle){
 	console.log("running");
-	// $(".dropbtn").on("click", function(){
-	// 	$("#menu-dropdown-content").toggle();
-	// });
+	$(".dropbtn").on("click", function(){
+		// if($("#menu-dropdown-content").css("display", "block")) {
+		// 	$("#menu-dropdown-content").slideUp(200);
+		// }
+		$("#menu-dropdown-content").slideToggle(200);
+	});
+
+	$("#menu-dropdown-content a").on("click", function(){
+		$("#menu-dropdown-content").css("display", "none");
+	});
+
+
 	$(document).on("click", function(e){
 		console.log("window clicked");
 		var button = $(".dropbtn");
 		if (!button.is(e.target) && button.has(e.target).length === 0) {
-			$("#menu-dropdown-content").css("display", "none");
+			$("#menu-dropdown-content").slideUp(200);
 		} else {
-			$("#menu-dropdown-content").css("display", "block");
+			// $("#menu-dropdown-content").slideDown(200);
 		}
 	});
 	
